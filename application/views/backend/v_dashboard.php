3a087f84cf19039a80259fb0ac477f15fbd12e74
Andi Personal <andidev30.personal@gmail.com>
2021-06-22 16:18:27 +0700

    fix bug

--- application/views/backend/v_dashboard.php
+++ application/views/backend/v_dashboard.php
@@ -226,7 +226,7 @@ $jum_konfirmasi = $query3->num_rows();
 				<!-- Main row -->
 				<div class="row">
 					<!-- Left col -->
-					<div class="col-md-8">
+					<div class="col-md-10">
 						<!-- MAP & BOX PANE -->
 						<div class="box box-success">
 							<div class="box-header with-border">
@@ -262,6 +262,27 @@ $jum_konfirmasi = $query3->num_rows();
 						<!-- /.box -->
 					</div>
 					<!-- /.col -->
+				</div>
+
+				<div class="row">
+					<div class="col-md-6">
+						<div class="box box-success">
+							<div class="box-header with-border">
+								<h3 class="box-title">Bar Chart</h3>
+
+								<div class="box-tools pull-right">
+									<button type="button" class="btn btn-box-tool" data-widget="collapse"><i class="fa fa-minus"></i>
+									</button>
+									<button type="button" class="btn btn-box-tool" data-widget="remove"><i class="fa fa-times"></i></button>
+								</div>
+							</div>
+							<div class="box-body">
+								<div class="chart">
+									<canvas id="barChart" style="height:230px"></canvas>
+								</div>
+							</div>
+						</div>
+					</div>
 
 					<div class="col-md-4">
 						<?php
@@ -270,7 +291,7 @@ $jum_konfirmasi = $query3->num_rows();
 						coalesce(count(case when status = 'LUNAS' then orders.id_order end), 0) as count_lunas,
 						coalesce(sum(case when status = 'LUNAS' then
 						((ceiling((kembali - ambil+1) / 3) * harga_product) +
-                    	(ceiling((kembali - ambil+1) / 3) * harga_deposit)) * quantity end),0) as total_lunas,
+						(ceiling((kembali - ambil+1) / 3) * harga_deposit)) * quantity end),0) as total_lunas,
 						coalesce(count(case when is_kembali = 0 then 1 end), 0) as count_belum_dikembalikan
 						from orders
 						left join
@@ -354,9 +375,8 @@ $jum_konfirmasi = $query3->num_rows();
 
 							<!-- PRODUCT LIST -->
 						<?php endforeach; ?>
-						<!-- /.box -->
+					
 					</div>
-					<!-- /.col -->
 				</div>
 				<!-- /.row -->
 			</section>
@@ -547,6 +567,81 @@ $jum_konfirmasi = $query3->num_rows();
 			pdf.download('Dashboard.pdf');
 		}
 	</script>
+<script src="https://adminlte.io/themes/AdminLTE/bower_components/chart.js/Chart.js"></script>
+<script>
+  $(function () {
+    /* ChartJS
+     * -------
+     * Here we will create a few charts using ChartJS
+     */
+
+    var areaChartData = {
+      labels  : ['January', 'February', 'March', 'April', 'May', 'June', 'July'],
+      datasets: [
+        {
+          label               : 'Electronics',
+          fillColor           : 'rgba(210, 214, 222, 1)',
+          strokeColor         : 'rgba(210, 214, 222, 1)',
+          pointColor          : 'rgba(210, 214, 222, 1)',
+          pointStrokeColor    : '#c1c7d1',
+          pointHighlightFill  : '#fff',
+          pointHighlightStroke: 'rgba(220,220,220,1)',
+          data                : [65, 59, 80, 81, 56, 55, 40]
+        },
+        {
+          label               : 'Digital Goods',
+          fillColor           : 'rgba(60,141,188,0.9)',
+          strokeColor         : 'rgba(60,141,188,0.8)',
+          pointColor          : '#3b8bba',
+          pointStrokeColor    : 'rgba(60,141,188,1)',
+          pointHighlightFill  : '#fff',
+          pointHighlightStroke: 'rgba(60,141,188,1)',
+          data                : [28, 48, 40, 19, 86, 27, 90]
+        }
+      ]
+    }
+
+    //-------------
+    //- BAR CHART -
+    //-------------
+    var barChartCanvas                   = $('#barChart').get(0).getContext('2d')
+    var barChart                         = new Chart(barChartCanvas)
+    var barChartData                     = areaChartData
+    barChartData.datasets[1].fillColor   = '#00a65a'
+    barChartData.datasets[1].strokeColor = '#00a65a'
+    barChartData.datasets[1].pointColor  = '#00a65a'
+    var barChartOptions                  = {
+      //Boolean - Whether the scale should start at zero, or an order of magnitude down from the lowest value
+      scaleBeginAtZero        : true,
+      //Boolean - Whether grid lines are shown across the chart
+      scaleShowGridLines      : true,
+      //String - Colour of the grid lines
+      scaleGridLineColor      : 'rgba(0,0,0,.05)',
+      //Number - Width of the grid lines
+      scaleGridLineWidth      : 1,
+      //Boolean - Whether to show horizontal lines (except X axis)
+      scaleShowHorizontalLines: true,
+      //Boolean - Whether to show vertical lines (except Y axis)
+      scaleShowVerticalLines  : true,
+      //Boolean - If there is a stroke on each bar
+      barShowStroke           : true,
+      //Number - Pixel width of the bar stroke
+      barStrokeWidth          : 2,
+      //Number - Spacing between each of the X value sets
+      barValueSpacing         : 5,
+      //Number - Spacing between data sets within X values
+      barDatasetSpacing       : 1,
+      //String - A legend template
+      legendTemplate          : '<ul class="<%=name.toLowerCase()%>-legend"><% for (var i=0; i<datasets.length; i++){%><li><span style="background-color:<%=datasets[i].fillColor%>"></span><%if(datasets[i].label){%><%=datasets[i].label%><%}%></li><%}%></ul>',
+      //Boolean - whether to make the chart responsive
+      responsive              : true,
+      maintainAspectRatio     : true
+    }
+
+    barChartOptions.datasetFill = false
+    barChart.Bar(barChartData, barChartOptions)
+  })
+</script>
 
 </body>
 
